45f808f987fa62e77fbd64987ef5fe65bf69e739
Ibrahim Eren Kilisli <ierenkilisli@gmail.com>
2025-02-21 17:50:18 +0300

    jump fixed

--- src/app.js
+++ src/app.js
@@ -1,27 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-const car = document.querySelector('#car');
-let position = 0;
-let gravity = 0.7;
-
+    const car = document.querySelector('#car');
+    const grid = document.querySelector('#grid');
+    let position = 0;      
+    let isJumping = false; 
+    
     function control(e) {
-        if (e.code === 'Space') {
+        if (e.code === "Space" && !isJumping) {
             jump();
         }
     }
     
     function jump() {
-        let upInterval = setInterval(() => {
-            if (position >= 150) {
-
-                clearInterval(upInterval);
+        isJumping = true;
     
+
+        let upInterval = setInterval(() => {
+
+            if (position >= 150) {
+                clearInterval(upInterval);
+                
+
                 let downInterval = setInterval(() => {
                     if (position <= 0) {
                         clearInterval(downInterval);
+                        isJumping = false; 
                     }
                     position -= 5;
                     car.style.bottom = position + 'px';
                 }, 20);
+    
             } else {
                 position += 5;
                 car.style.bottom = position + 'px';
@@ -30,4 +37,4 @@ let gravity = 0.7;
     }
     
     document.addEventListener('keydown', control);
-});
+    });
