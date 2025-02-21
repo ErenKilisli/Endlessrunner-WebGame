e4d0e61612d8ecd2c965ff9fdc6b061cfc6f9184
Ibrahim Eren Kilisli <ierenkilisli@gmail.com>
2025-02-21 18:14:08 +0300

    obstacle init

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const car = document.querySelector('#car');
-    const grid = document.querySelector('#grid');
-    let position = 0;      
-    let isJumping = false; 
+const car = document.querySelector('#car');
+const grid = document.querySelector('#grid');
+let position = 0;      
+let isJumping = false; 
     
-    function control(e) {
-        if (e.code === "Space" && !isJumping) {
-            jump();
-        }
+function control(e) {
+    if (e.code === "Space" && !isJumping) {
+        jump();
     }
+}
     
-    function jump() {
-        isJumping = true;
+function jump() {
+    isJumping = true;
     
 
-        let upInterval = setInterval(() => {
+    let upInterval = setInterval(() => {
 
-            if (position >= 150) {
-                clearInterval(upInterval);
+        if (position >= 150) {
+            clearInterval(upInterval);
                 
 
                 let downInterval = setInterval(() => {
@@ -35,6 +35,21 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }, 20);
     }
+
+    function generateObstacles() {
+        let obstaclePosition = 1000
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        grid.appendChild(obstacle)
+        obstacle.style.left = obstaclePosition + 'px'
+
+        let timerID = setInterval(function () {
+            obstaclePosition -= 10
+            obstacle.style.left = obstaclePosition + 'px'
+        }, 20)
+    }
+
+    generateObstacles()
     
     document.addEventListener('keydown', control);
     });
